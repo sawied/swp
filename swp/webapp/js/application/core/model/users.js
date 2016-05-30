172a01bfca94b35638893afdab88544641d81fa9
James X W Zhang <danan.2009@hotmail.com>
2016-05-30 12:57:12 +0800

    demonstrate how to use pipe

--- swp/webapp/js/application/core/model/users.js
+++ swp/webapp/js/application/core/model/users.js
@@ -3,7 +3,19 @@ define(["jquery","core/service"],function($,service){
 	
 	
 	 var loadAll = function(param){
-		 return service("retrieveUsers",param);
+		 return service("retrieveUsers",param).pipe(function (data) {
+			 
+			var groups= $.map(data.data,function (item) {
+
+				 var group={id:item.id,name:item.name,birthday:item.birthday,comments:item.comments};
+				 return $.extend(group,{json:JSON.stringify(group)});
+
+			 });
+
+			 return {data:groups};
+
+
+		 });
 	 };
 
 	return {
