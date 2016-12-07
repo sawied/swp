4ebd00201c4137d395ffdaa7707a4141c2d32676
DANAN <danan.2009@hotmail.com>
2016-12-07 23:16:32 +0800

    Reactjs project init

---
+++ jweb/src/components/FileManager.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class FileManager extends React.Component {
+  render() {
+    return (
+      <div>FileManager component</div>
+    );
+  }
+}
+
+export default FileManager;
