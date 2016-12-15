ea760e7ae90ce6c2f74664a6ea81120e69c4fe39
zhangxiaowei <danan.2009@hotmail.com>
2016-12-15 17:40:50 +0800

    init websocket project.

---
+++ websocket/src/main/java/com/github/sawied/websocket/App.java
@@ -0,0 +1,13 @@
+package com.github.sawied.websocket;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
