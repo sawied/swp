db3aa56f31dccc9eaf9f13328a28bd854b907e27
sawied <danan.2009@gmail.com>
2017-03-12 19:25:32 +0800

    some JPA implements

--- persistent/src/main/java/com/github/sawied/persistent/repository/UserAuditLogRepository.java
+++ persistent/src/main/java/com/github/sawied/persistent/repository/UserAuditLogRepository.java
@@ -1,8 +1,24 @@
 package com.github.sawied.persistent.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.ParameterExpression;
+import javax.persistence.criteria.Root;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.github.sawied.persistent.domain.UserAuditLog;
 
@@ -11,7 +27,42 @@ import com.github.sawied.persistent.domain.UserAuditLog;
  * @author DANAN
  *
  */
-@Repository
-public interface UserAuditLogRepository extends JpaRepository<UserAuditLog,Long>,JpaSpecificationExecutor<UserAuditLog> {
+public  class UserAuditLogRepository extends SimpleJpaRepository<UserAuditLog,Long> {
+    
+    private EntityManager em;
+
+    @Autowired
+    public UserAuditLogRepository(
+	    EntityManager em) {	
+	super(UserAuditLog.class,em);
+	this.em=em;
+    }
+    
+    @Transactional(propagation=Propagation.REQUIRED)
+    public Page<UserAuditLog> searchUserLog(Map<String,Object> params,Pageable pageable){
+	CriteriaBuilder cb = this.em.getCriteriaBuilder();
+	CriteriaQuery<UserAuditLog> query = cb.createQuery(UserAuditLog.class);
+	Root<UserAuditLog> root = query.from(UserAuditLog.class);
+	query.select(root);
+	
+	ParameterExpression<String> p = cb.parameter(String.class, "message");
+	query.where(cb.like(root.<String>get("message"), p));
+	
+	TypedQuery<UserAuditLog> q = em.createQuery(query);
+	Set<String> keys = params.keySet();
+	Iterator<String> iterator = keys.iterator();
+	while(iterator.hasNext()){
+	    String key = iterator.next();
+	    q.setParameter(key,params.get(key));
+	}
+	
+	List<UserAuditLog> result = q.getResultList();
+	
+	return new PageImpl<UserAuditLog>(result, pageable, result.size());
+    }
+    
+    
+    
+    
 
 }
--- persistent/src/test/java/com/github/sawied/persistent/repositoryTest/UserAuditLogTest.java
+++ persistent/src/test/java/com/github/sawied/persistent/repositoryTest/UserAuditLogTest.java
@@ -1,60 +1,50 @@
 package com.github.sawied.persistent.repositoryTest;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
+import java.util.HashMap;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import com.github.sawied.persistent.domain.UserAuditLog;
 import com.github.sawied.persistent.repository.UserAuditLogRepository;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:sawied/persistent/test/persistent-test-context.xml"})
+@ContextConfiguration(locations = { "classpath:sawied/persistent/test/persistent-test-context.xml" })
 public class UserAuditLogTest {
-	
-	@Autowired
-	private UserAuditLogRepository userAuditLogRepository;
-	
-	
-	@BeforeClass
-	public static void setup(){
-		
-	}
-	
-	@Test
-	public void createUserAuditLogTest(){
-		Assert.assertNotNull(userAuditLogRepository);
-		UserAuditLog userAuditLog = new UserAuditLog();
-		userAuditLog.setCode((short) 400);
-		userAuditLog.setMessage("invalid invoke.");
-		userAuditLogRepository.save(userAuditLog);
-		Assert.assertEquals(1, userAuditLogRepository.count());
-	}
-	
-	
-	public void searchAuditLogTest(){
-		userAuditLogRepository.findAll(new Specification<UserAuditLog>(){
 
-			@Override
-			public Predicate toPredicate(Root<UserAuditLog> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-				
-				return cb.like(root.get("message"), "xx");
-			}
-			
-		}, new PageRequest(0,10));
-	}
-	
-	
-	
+    @Autowired
+    private UserAuditLogRepository userAuditLogRepository;
+
+    @BeforeClass
+    public static void setup() {
+
+    }
+
+    @Test
+    public void createUserAuditLogTest() {
+	Assert.assertNotNull(userAuditLogRepository);
+	UserAuditLog userAuditLog = new UserAuditLog();
+	userAuditLog.setCode((short) 400);
+	userAuditLog.setMessage("0");
+	userAuditLogRepository.save(userAuditLog);
+	Assert.assertEquals(1, userAuditLogRepository.count());
+    }
+
+    @Test
+    public void searchAuditLogTest() {
+	PageRequest page = new PageRequest(10, 1);
+	HashMap<String, Object> map = new HashMap<String, Object>();
+	map.put("message", "0");
+	Page<UserAuditLog> result = userAuditLogRepository.searchUserLog(map, page);
+	Assert.assertEquals(1, result.getSize());
+
+    }
 
 }
