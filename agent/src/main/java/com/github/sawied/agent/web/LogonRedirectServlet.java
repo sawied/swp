de5550bf6bd5e153954a5df55bbae90b99181d85
zhangxiaowei <danan.2009@hotmail.com>
2017-03-13 18:33:31 +0800

    logon and JPA Search

---
+++ agent/src/main/java/com/github/sawied/agent/web/LogonRedirectServlet.java
@@ -0,0 +1,38 @@
+package com.github.sawied.agent.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LogonRedirectServlet
+ */
+public class LogonRedirectServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogonRedirectServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println(request.getParameter("logonType"));
+		response.sendRedirect("/logon.html");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
--- persistent/src/main/java/com/github/sawied/persistent/repository/UserAuditLogRepository.java
+++ persistent/src/main/java/com/github/sawied/persistent/repository/UserAuditLogRepository.java
@@ -1,5 +1,6 @@
 package com.github.sawied.persistent.repository;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -10,8 +11,10 @@ import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.ParameterExpression;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
+import org.apache.commons.collections.IteratorUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -38,27 +41,69 @@ public  class UserAuditLogRepository extends SimpleJpaRepository<UserAuditLog,Lo
 	this.em=em;
     }
     
-    @Transactional(propagation=Propagation.REQUIRED)
-    public Page<UserAuditLog> searchUserLog(Map<String,Object> params,Pageable pageable){
-	CriteriaBuilder cb = this.em.getCriteriaBuilder();
-	CriteriaQuery<UserAuditLog> query = cb.createQuery(UserAuditLog.class);
-	Root<UserAuditLog> root = query.from(UserAuditLog.class);
-	query.select(root);
-	
-	ParameterExpression<String> p = cb.parameter(String.class, "message");
-	query.where(cb.like(root.<String>get("message"), p));
-	
-	TypedQuery<UserAuditLog> q = em.createQuery(query);
-	Set<String> keys = params.keySet();
-	Iterator<String> iterator = keys.iterator();
-	while(iterator.hasNext()){
-	    String key = iterator.next();
-	    q.setParameter(key,params.get(key));
+	@SuppressWarnings("unchecked")
+	@Transactional(propagation = Propagation.REQUIRED)
+	public Page<UserAuditLog> searchUserLog(Map<String, Object> params, Pageable pageable) {
+		// Query Count
+		Long count = countByCondition(params);
+
+		// Query Result List
+		CriteriaBuilder cb = this.em.getCriteriaBuilder();
+		CriteriaQuery<UserAuditLog> query = cb.createQuery(UserAuditLog.class);
+		Root<UserAuditLog> root = query.from(UserAuditLog.class);
+		query.select(root);
+		List<Predicate> predicates=buildPridicate(params,root,cb);
+    	if(!predicates.isEmpty()){    	
+    		query.where(predicates.toArray(new Predicate[0]));
+    	}
+		TypedQuery<UserAuditLog> q = em.createQuery(query);
+		q.setMaxResults(pageable.getPageSize());
+		q.setFirstResult(pageable.getOffset());
+		if(pageable!=null&&pageable.getSort()!=null){			
+			query.orderBy(IteratorUtils.toList(pageable.getSort().iterator()));
+		}
+		List<UserAuditLog> result = this.applyParameters(params, q).getResultList();
+		return new PageImpl<UserAuditLog>(result, pageable, count);
 	}
-	
-	List<UserAuditLog> result = q.getResultList();
-	
-	return new PageImpl<UserAuditLog>(result, pageable, result.size());
+    
+    
+    private Long countByCondition(Map<String,Object> params){
+    	CriteriaBuilder cb = this.em.getCriteriaBuilder();
+    	CriteriaQuery<Long> cq=cb.createQuery(Long.class);
+    	Root<UserAuditLog> root=cq.from(UserAuditLog.class);
+    	cq.select(cb.count(root));
+    	List<Predicate> predicates=buildPridicate(params,root,cb);
+    	if(!predicates.isEmpty()){    	
+    		cq.where(predicates.toArray(new Predicate[0]));
+    	}
+    	
+    	return this.applyParameters(params, this.em.createQuery(cq)).getSingleResult();
+    }
+    
+    private <T>TypedQuery<T> applyParameters(Map<String,Object> params,TypedQuery<T> q){
+    	Set<String> keys = params.keySet();
+    	Iterator<String> iterator = keys.iterator();
+    	while(iterator.hasNext()){
+    	    String key = iterator.next();
+    	    q.setParameter(key,params.get(key));
+    	}
+    	return q;
+    }
+    
+    
+    /**
+     * 
+     * @param params
+     * @param cb
+     * @return
+     */
+    private List<Predicate> buildPridicate(Map<String,Object> params,Root<UserAuditLog> root,CriteriaBuilder cb){
+    	List<Predicate> list = new ArrayList<Predicate>();
+    	if(params.containsKey("message")){
+    		 ParameterExpression<String> p=cb.parameter(String.class, "message");
+    		 list.add(cb.like(root.<String>get("message"), p));
+    	}
+    	return list;
     }
     
     
