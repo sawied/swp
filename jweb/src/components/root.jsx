fef15b6e6a8f7c4dd11942af199561ba5104189f
sawied <danan.2009@gmail.com>
2017-08-28 22:30:59 +0800

    add some changes

--- jweb/src/components/root.jsx
+++ jweb/src/components/root.jsx
@@ -1,19 +1,13 @@
-import 'core-js/fn/object/assign';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {createStore,applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
 import {Provider} from 'react-redux';
-import App from './components/AppCore';
-import Home from './components/Home';
-import FileManager from './components/FileManager';
-import Todos from './todos/todos';
-import TodosReducers from './todos/reducers';
+import App from 'AppCore';
+import Home from 'Home';
+import FileManager from 'FileManager';
+import Todos from 'todos';
+import TodosReducers from './../reducers';
 import {Router,Route,browserHistory,IndexRoute} from 'react-router';
-import createDefaultStore from '../stores/defaultStore';
+import createDefaultStore from './../stores/defaultStore';
 
-
+let store = createDefaultStore(TodosReducers);
 export default class extends Component{
   render(){
     return (<Provider store={store}>
@@ -27,17 +21,3 @@ export default class extends Component{
     </Provider>);
   }
 }
-let store = createDefaultStore(TodosReducers);
-
-// Render the main component into the dom
-ReactDOM.render(
-  <Provider store={store}>
-  <Router history={browserHistory}>
-    <Route path='/' component={App}>
-      <IndexRoute component={Home}/>
-      <Route path="/todos" component={Todos}/>
-      <Route path='/fileManager' component={FileManager}/>
-    </Route>
-  </Router>
-</Provider>
-, document.getElementById('app-container'));
