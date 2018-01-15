62c4148ac7cd074e53304dfdbdd4eb02f59048b0
zhangxiaowei <danan.2009@hotmail.com>
2018-01-15 11:00:17 +0800

    test jceks keystore compatible with IBM and oracle jdk

--- microservice-client/src/test/java/com/github/sawied/microservice/oauth2/client/test/DesedeEncryptTest.java
+++ microservice-client/src/test/java/com/github/sawied/microservice/oauth2/client/test/DesedeEncryptTest.java
@@ -7,6 +7,7 @@ import java.security.KeyStore;
 import java.security.KeyStore.SecretKeyEntry;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.Security;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
 
@@ -32,7 +33,7 @@ public class DesedeEncryptTest {
 	
 	private Resource keystore_resource = new ClassPathResource("sawied/microservice/client/key/keystore.p12");
 	
-	private Resource sun_keystore_resource = new ClassPathResource("sawied/microservice/client/key/keystore_SUN.jks");
+	private Resource sun_keystore_resource = new ClassPathResource("sawied/microservice/client/key/key.jceks");
 	
 	
 	private byte[] secureKey= new byte[] {21, 47, -15, 32, -108, -27, -122, 107, -63, 61, -33, -85, -83, -123, 62, -60, -92, 81, -125, 35, -108, -29, 2, -105};
@@ -41,35 +42,27 @@ public class DesedeEncryptTest {
 	
 	
 	@Test
-	@Ignore
-	public void samekeyTest() throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException, UnrecoverableKeyException {
+	public void samekeyTest() throws Exception {
 		KeyStore keyStore = KeyStore.getInstance("jceks");
-		keyStore.load(ibm_keystore_resource.getInputStream(), "212121_vtm_keystore_password_121212".toCharArray());
-		Key key = keyStore.getKey("vtm_logon_encrypt_pk", "101010_vtm_key_password_010101".toCharArray());
-		
-		System.out.println(Base64.encodeBase64String(key.getEncoded()));
-		
-		KeyStore jkskeyStore = KeyStore.getInstance("pkcs12");
-		jkskeyStore.load(null,null);
-		SecretKeyEntry keyEntry = new KeyStore.SecretKeyEntry((SecretKey) key);
+		keyStore.load(null,null);
+		SecretKeyEntry keyEntry = new KeyStore.SecretKeyEntry((SecretKey) toKey(this.secureKey));
 		char[] password = PASSWORD.toCharArray();
 	    KeyStore.ProtectionParameter protParam = 
 	    new KeyStore.PasswordProtection(password);
-		jkskeyStore.setEntry("client-key", keyEntry, protParam);
-		//jkskeyStore.setKeyEntry("client-key", key, null, null);
+	    keyStore.setEntry("client-key", keyEntry, protParam);
 		
 		FileOutputStream stream = new FileOutputStream("D:/works/sawied/key.p12");
-		jkskeyStore.store(stream, PASSWORD.toCharArray());
+		keyStore.store(stream, PASSWORD.toCharArray());
 		stream.close();
 	}
 	
 	@Test
 	public void encryptStrTest() throws Exception {
 		
-	    KeyStore keyStore = KeyStore.getInstance("PKCS12");
-		keyStore.load(keystore_resource.getInputStream(), "hacn1234".toCharArray());
+	    KeyStore keyStore = KeyStore.getInstance("jceks");
+	    
+		keyStore.load(sun_keystore_resource.getInputStream(), "hacn1234".toCharArray());
 		Key key = keyStore.getKey("client-key", "hacn1234".toCharArray());
-		key=toKey(key.getEncoded());
 		Cipher cipher=Cipher.getInstance(KEYALG);
 		cipher.init(Cipher.DECRYPT_MODE,key);
 		String result=new String((cipher.doFinal(Base64.decodeBase64("irUJGfa+Nq5sX4TOVZyZCxEtB4NiZ8dg/+lphsNt24c="))));
