c49a1c618875f5808777728a5a1dc3a169c4aae1
zhangxiaowei <danan.2009@hotmail.com>
2018-01-29 15:23:41 +0800

    remove unused configurations for oauth2

--- microservice-apigateway/src/main/java/com/github/sawied/microservice/oauth2/zuul/config/ZuulApiGatewayConfig.java
+++ microservice-apigateway/src/main/java/com/github/sawied/microservice/oauth2/zuul/config/ZuulApiGatewayConfig.java
@@ -9,29 +9,7 @@ import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 @Configuration
-public class ZuulApiGatewayConfig extends AbstractAnnotationConfigDispatcherServletInitializer implements WebApplicationInitializer{
-
-	@Override
-	protected Class<?>[] getRootConfigClasses() {
-		return null;
-	}
-
-	@Override
-	protected Class<?>[] getServletConfigClasses() {
-		return new Class<?>[] {ZuulApiGatewayServletConfig.class};
-	}
-
-	@Override
-	protected String[] getServletMappings() {
-		return new String[] {"/"};
-	}
-	
-	@Override
-	public void onStartup(ServletContext servletContext) throws ServletException {
-		super.onStartup(servletContext);
-		DelegatingFilterProxy filter = new DelegatingFilterProxy("oauth2ClientContextFilter");
-		servletContext.addFilter("oauth2ClientContextFilter", filter).addMappingForUrlPatterns(null, false, "/*");
-	}
+public class ZuulApiGatewayConfig{
 	
 
 }
